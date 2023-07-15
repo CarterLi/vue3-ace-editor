e2a667b2b705333ed2614cc1ffa27dec0ec920f1
李通洲 <zhangsongcui3371@sina.com>
2023-07-15 13:35:06 +0800

    Make vue and ace-editor peer dependencies
    Fix #41

--- index.d.ts
+++ index.d.ts
@@ -26,35 +26,35 @@ export declare const VAceEditor: import("vue").DefineComponent<{
     focus(this: VAceEditorInstance): void;
     blur(this: VAceEditorInstance): void;
     selectAll(this: VAceEditorInstance): void;
-}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, string[], string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<{
-    value?: unknown;
-    lang?: unknown;
-    theme?: unknown;
-    options?: unknown;
-    placeholder?: unknown;
-    readonly?: unknown;
-    wrap?: unknown;
-    printMargin?: unknown;
-    minLines?: unknown;
-    maxLines?: unknown;
-} & {
-    value: string;
-    theme: string;
-    readonly: boolean;
-    wrap: boolean;
-    printMargin: number | boolean;
-    lang: string;
-} & {
-    options?: Record<string, any> | undefined;
-    placeholder?: string | undefined;
-    minLines?: number | undefined;
-    maxLines?: number | undefined;
-}> & {
-    [x: string & `on${string}`]: ((...args: any[]) => any) | undefined;
+}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, string[], string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<import("vue").ExtractPropTypes<{
+    value: {
+        type: StringConstructor;
+        required: true;
+    };
+    lang: {
+        type: StringConstructor;
+        default: string;
+    };
+    theme: {
+        type: StringConstructor;
+        default: string;
+    };
+    options: ObjectConstructor;
+    placeholder: StringConstructor;
+    readonly: BooleanConstructor;
+    wrap: BooleanConstructor;
+    printMargin: {
+        type: (BooleanConstructor | NumberConstructor)[];
+        default: boolean;
+    };
+    minLines: NumberConstructor;
+    maxLines: NumberConstructor;
+}>> & {
+    [x: `on${Capitalize<string>}`]: ((...args: any[]) => any) | undefined;
 }, {
+    lang: string;
     theme: string;
     readonly: boolean;
     wrap: boolean;
     printMargin: number | boolean;
-    lang: string;
-}>;
+}, {}>;
